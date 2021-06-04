c2268c8276bc36ebb321ba4f4bddba25453297d8
L-Dudina <dudina.luba88@gmail.com>
2021-06-04 19:12:45 +0300

    create: adaptive tabs

--- js/main.js
+++ js/main.js
@@ -5,11 +5,21 @@ $(document).ready(function () {
 
   // $('.newsletter').parallax({imageSrc: './img/newsletter-bg.jpg'});
 
-var menuButton = $(".menu-button"); 
-menuButton.on("click", function () {
-  $(".navbar-menu").toggleClass("navbar-menu--visible");
-});
+  var menuButton = $(".menu-button"); 
+  menuButton.on("click", function () {
+    $(".navbar-menu").toggleClass("navbar-menu--visible");
+  });
 
+  var tabsItem = $(".tabs__item");
+  var contentItem = $(".content__item");
+
+  tabsItem.on('click', function(event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("tabs__item--active");
+    contentItem.removeClass("content__item--active");
+    $(activeContent).addClass("content__item--active");
+    $(this).addClass("tabs__item--active");
+  });
 
   AOS.init();
 });
