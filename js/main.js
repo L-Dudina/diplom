a19c225129230d5e482193b835898bffc61fca3a
L-Dudina <dudina.luba88@gmail.com>
2021-06-05 16:37:06 +0300

    create: parallax section

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
 
   });
 
-  // $('.newsletter').parallax({imageSrc: './img/newsletter-bg.jpg'});
-
+ 
   var menuButton = $(".menu-button"); 
   menuButton.on("click", function () {
     $(".navbar-menu").toggleClass("navbar-menu--visible");
